03c41c9ad6a2b8c8d7a007526d95a3317f8328e8
Maksym Rudenko <marudenko96@gmail.com>
2025-06-07 14:49:02 +0200

    Fix CORS headers in generate-syllabus function

--- supabase/functions/generate-syllabus/index.ts
+++ supabase/functions/generate-syllabus/index.ts
@@ -41,7 +41,7 @@ interface GeneratedSyllabus {
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-client-info',
 }
 
 Deno.serve(async (req: Request) => {
